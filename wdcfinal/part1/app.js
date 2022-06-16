8831ee09ed66ea35ae43b1f03a80e808241751cc
TidalJ <395478675@qq.com>
2022-06-16 14:51:14 +0800

    change the seats table

--- wdcfinal/part1/app.js
+++ wdcfinal/part1/app.js
@@ -57,7 +57,7 @@ app.get('/searchorder', function (req, res) {
 })
 
 app.get('/ava', function (req, res) {
-  connection.query('SELECT * FROM availableseats', function (error, results, fields) {
+  connection.query('SELECT * FROM seats WHERE ordertime IS NULL', function (error, results, fields) {
     if (error) throw error;
     var deal = JSON.parse(JSON.stringify(results));
     // console.log('The solution is: ', deal);
@@ -70,10 +70,10 @@ app.get('/order', function (req, res) {
   // console.log(req.query);
   let data = req.query.dataselect;
   let section = req.query.sectionselect;
-  // console.log(data);
-  // console.log(section);
+  console.log(data);
+  console.log(section);
   let searchsql = 
-    connection.query('SELECT * FROM availableseats', function (error, results, fields) {
+    connection.query('SELECT * FROM seats WHERE ordertime IS NULL', function (error, results, fields) {
     if (error) throw error;
     var deal = JSON.parse(JSON.stringify(results));
     // console.log('The solution is: ', deal);
